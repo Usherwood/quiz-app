6b71cde19cb60c0860f79d4da8bd2c41ca0801f3
Usherwood <peter.usherwood@kantar.com>
2021-04-05 16:28:26 -0300

    copying tutorial basics

---
+++ my-app/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <LandingPage />
+  </div>
+</template>
+
+<script>
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+import LandingPage from './pages/LandingPage'
+
+export default {
+  name: 'App',
+  components: {
+    LandingPage
+  }
+}
+</script>
+
+<style>
+  @import './assets/styles/global.css';
+</style>
